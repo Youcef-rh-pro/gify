4eb4c8c15a02f3242a718b353b919081238217df
Youcef <youcefriah04@gmail.com>
2024-10-13 01:57:41 +0100

    using await async insted of promises

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
-  const images= document.getElementById('images')
-  const catBtn=document.getElementById('form')
-  catBtn.addEventListener('click',(e)=>{
-  images.innerHTML=''
-  e.preventDefault()
- const img = document.createElement('img');
- const search=document.getElementById('search').value
-    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=0V5EjxV7zAjuxvk8vX2qYwT2vSoCqhGo&s=${search}`, {mode: 'cors'})
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(response) {
-        if(response.data.constructor===Array){
-          images.innerHTML='no result'
+const images = document.getElementById("images");
+const catBtn = document.getElementById("form");
+catBtn.addEventListener("click", (e) => {
+  images.innerHTML = "";
+  e.preventDefault();
+  const img = document.createElement("img");
 
-        }else{
-          img.src=response.data.images.original.url
+  const search = document.getElementById("search").value;
 
-        }
-      })
-      .catch(error=>{
-        images.innerHTML='mistake happend'
-      }
-      );
-     images.append(img)
-})
+  getCat(img,search);
+
+  images.append(img);
+});
+async function getCat(img,search) {
+  const response = await fetch(
+    `https://api.giphy.com/v1/gifs/translate?api_key=0V5EjxV7zAjuxvk8vX2qYwT2vSoCqhGo&s=${search}`,
+    { mode: "cors" }
+  );
+
+  const catData = await response.json();
+
+  img.src = catData.data.images.original.url;
+
+  // .then(function(response) {
+  //   if(response.data.constructor===Array){
+  //     images.innerHTML='no result'
+
+  //   }else{
+  //
+
+  //   }
+  // })
+  // .catch(error=>{
+  //   images.innerHTML='mistake happend'
+  // }
+  // );
+}
